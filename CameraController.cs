cb3576396b766ce9f6b059c2ff22d45aa67c0f43
The-Graze <reece2004f@gmail.com>
2023-07-22 02:54:38 +0100

    Add project files.

---
+++ CameraController.cs
@@ -0,0 +1,33 @@
+﻿using Il2CppSystem;
+using Squido.NMR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace NMRB_CameraMod
+{
+     class CameraController : MonoBehaviour
+     {
+        Camera cam;
+        void Update()
+        {
+            if (gameObject.GetComponent<Character>()._mainCamera != null && cam == null)
+            {
+                MakeCam();
+            }
+        }
+        void MakeCam()
+        {
+            cam = GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent<Camera>();
+            cam.stereoTargetEye = StereoTargetEyeMask.None;
+            cam.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+            cam.transform.SetParent(transform);
+            cam.allowDynamicResolution = true;
+            cam.targetDisplay = 8;
+            cam.fieldOfView = 85;
+            cam.transform.localPosition = new Vector3(0, -5, 0);
+        }
+     }
+}
---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+using Squido.NMR;
+using UnhollowerRuntimeLib;
+
+namespace NMRB_CameraMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        CameraController Camcontroller;
+        Harmony harmony;
+        public override void Load()
+        {
+            // Plugin startup logic
+            ClassInjector.RegisterTypeInIl2Cpp(typeof(CameraController));
+            harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+        [HarmonyPatch(typeof(Character), nameof(Character.Awake))]
+        class Patch
+        {
+            static void Postfix(Character __instance)
+            {
+                __instance.gameObject.AddComponent<CameraController>();
+            }
+        }
+    }
+}
